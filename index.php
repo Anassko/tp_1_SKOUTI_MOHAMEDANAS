3a663a9276281c6ef670ddf1301a0c87fe85e3fb
Anas <medanas.skouti@gmail.com>
2023-10-06 16:44:04 -0400

    init

--- index.php
+++ index.php
@@ -1,14 +1,27 @@
 <?php
 function validerMotDePasse($motDePasse){
+
+    //longueur du mot de passe
+
     if (strlen($motDePasse) < 6 || strlen($motDePasse) > 10) {
         return "veuillez entrer un mot de passe entre 6 et 10 caracteres.";
 }
 
+// definir le sel
 $sel = "ABCD12345@"
 
+// concatenation du sel au mdp
+
 $motDePasseAvecSel = $motDePasse . $sel;
+
+// chiffrement avec hachage
+
 $motDePasseChiffre = password_hash($motdePasseAvecSel, PASSWORD_BCRYPT);
+
+//mot de passe correct de test
 $motDePasseCorrect = "MotDePasse1234";
+
+// Vérifier si le mot de passe est correct
 
 if(password_verify($motdePasseCorrect . $sel, $motDePasseChiffre)) {
     return "le Mot de passe correct : Sel = $sel, Mot de passe chiffre = $motDePasseChiffre ";
@@ -18,8 +31,8 @@ if(password_verify($motdePasseCorrect . $sel, $motDePasseChiffre)) {
 
 }
 
+// Test fonction
 $motDePasseTest = "MotDePasse123";
 $resultat = validerMotDePasse($motDePasseTest);
 echo $resultat;
-
 ?>
